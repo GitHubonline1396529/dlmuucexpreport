d11810cda1a9cd0f68ea68caaed8dd95a0ef683d
User1396529 <yyg249942899@163.com>
2024-08-21 19:04:17 +0800

    补充了代码文件的 DocString 以便更好地展示代码高亮功能

--- code/demo.py
+++ code/demo.py
@@ -1,7 +1,22 @@
+"""
+demo.py - 示例代码
+==================
+
+一份示例代码，基于 :module:`scipy.cluster.hierarchy` 模块实现了系统聚类，
+用于测试代码块高亮渲染和排版效果。聚类的数据来源于随即数据。
+
+使用方法：
+
+1. 直接运行代码即可查看代码运行结果。
+2. 使用高亮渲染工具查看代码文件。
+"""
+
+# 导入必要的库
 import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.preprocessing import StandardScaler
 from scipy.cluster.hierarchy import linkage, dendrogram
-import matplotlib.pyplot as plt
 
 # 生成随机数据
 np.random.seed(42)
@@ -16,7 +31,11 @@ Z = linkage(data_normalized, method='ward')
 
 # 绘制树状图
 plt.figure(figsize=(10, 5))
-dendrogram(Z, leaf_rotation=90, leaf_font_size=10)
+dendrogram(
+    Z, 
+    leaf_rotation=90, 
+    leaf_font_size=10
+    )
 plt.title('Hierarchical Clustering Dendrogram')
 plt.xlabel('Sample index')
 plt.ylabel('Distance')
